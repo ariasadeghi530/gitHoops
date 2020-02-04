9b6ddb5bea76dc1402e164f77ad4460921db3e36
EvanErickson <evanserickson@gmail.com>
2020-02-04 14:27:04 -0800

    added two rows of 4

--- assets/js/teamsScript.js
+++ assets/js/teamsScript.js
@@ -26,4 +26,28 @@ fetch('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=1')
         document.getElementById('teamName4').innerHTML = `
             ${teams[3].strTeam}
         `
+        document.getElementById('teamPhoto5').innerHTML = `
+            <img src="${teams[4].strTeamBadge}">
+        `
+        document.getElementById('teamName5').innerHTML = `
+            ${teams[4].strTeam}
+        `
+        document.getElementById('teamPhoto6').innerHTML = `
+            <img src="${teams[5].strTeamBadge}">
+        `
+        document.getElementById('teamName6').innerHTML = `
+            ${teams[5].strTeam}
+        `
+        document.getElementById('teamPhoto7').innerHTML = `
+            <img src="${teams[6].strTeamBadge}">
+        `
+        document.getElementById('teamName7').innerHTML = `
+            ${teams[6].strTeam}
+        `
+        document.getElementById('teamPhoto8').innerHTML = `
+            <img src="${teams[7].strTeamBadge}">
+        `
+        document.getElementById('teamName8').innerHTML = `
+            ${teams[7].strTeam}
+        `
     })
