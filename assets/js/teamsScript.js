3ad554bb4bfad57e7839772a42dbd9f573cd259e
EvanErickson <evanserickson@gmail.com>
2020-02-04 14:05:24 -0800

    working on teams.html, trying to add multiple teams icons at top

--- assets/js/teamsScript.js
+++ assets/js/teamsScript.js
@@ -8,4 +8,22 @@ fetch('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Arsenal')
         document.getElementById('teamName').innerHTML = `
             ${teams[0].strTeam}
         `
+        document.getElementById('teamPhoto2').innerHTML = `
+            <img src="${teams[1].strTeamBadge}">
+        `
+        document.getElementById('teamName2').innerHTML = `
+            ${teams[1].strTeam}
+        `
+        document.getElementById('teamPhoto3').innerHTML = `
+            <img src="${teams[2].strTeamBadge}">
+        `
+        document.getElementById('teamName3').innerHTML = `
+            ${teams[2].strTeam}
+        `
+        document.getElementById('teamPhoto4').innerHTML = `
+            <img src="${teams[3].strTeamBadge}">
+        `
+        document.getElementById('teamName4').innerHTML = `
+            ${teams[3].strTeam}
+        `
     })
