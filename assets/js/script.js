1ff147e6ca9ce1d28ac33589796a94afee5c0004
EvanErickson <evanserickson@gmail.com>
2020-02-04 12:23:36 -0800

    made changes

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+fetch('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Arsenal')
+    .then(r => r.json())
+    .then(({teams}) => {
+        console.log(teams[0])
+        document.getElementById('teamName').innerHTML = `
+            ${teams[0].strTeam}
+        `
+    })
+    .catch(e => console.log(e))
+
+// document.getElementById('playerPhoto').innerHTML = 
+
+
+// fetch(`http://www.omdbapi.com/?t=${document.getElementById('title').value}&apikey=trilogy`)
+//         .then(r => r.json())
+//         .then(({ Title, Year, Director, Poster, Plot }) => {
+//           movie = {
+//             title: Title,
+//             year: Year,
+//             director: Director,
+//             plot: Plot
+//           }
+//           document.getElementById('movie').innerHTML = `
+//           <div class="card">
+//             <div class="card-image">
+//               <img src="${Poster}" alt="${Title}">
+//               <span class="card-title">${Title}</span>
+//             </div>
+//             <div class="card-content">
+//               <h4>Directed by ${Director}</h4>
+//               <h5>Year: ${Year}</h5>
+//               <p>${Plot}</p>
+//             </div>
+//             <div class="card-action">
+//               <button class="btn waves-effect waves-light addWatchlist">Add To Watchlist</button>
+//             </div>
+//           </div>
+//         `
+//         document.getElementById('title').value = ''
+//         })
+//         .catch(e => console.error(e))
