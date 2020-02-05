8aaa7cd1b7526906b555cd036af2b3f644909706
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-05 14:13:29 -0800

    linked to teams js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,12 @@
-//array containing names of all the teams
-let nbaTeamNames = [];
-
-//populate array with all team names
-fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NBA')
-.then(r => r.json())
-.then(({teams}) => {
-  teams.forEach((element) => {
-    nbaTeamNames.push(element.strTeam.toLowerCase());
-    let teamNameArr = element.strTeam.split(" ");
-    
-    if (teamNameArr.length > 2) {
-  
-      nbaTeamNames.push(teamNameArr[teamNameArr.length - 1].toLowerCase())
-    } else {
-      nbaTeamNames.push(teamNameArr[teamNameArr.length - 1].toLowerCase())
-    }
-    
-  });
- 
-})
-.catch(e => console.error(e));
 
 
-//search bar redirect
 
 let searchVal = localStorage.getItem('search') || '';
 
 
+//search bar redirect
 document.getElementById('searchBtn').addEventListener("click", event => {
+  
 searchVal = document.getElementById("searchBar").value.toLowerCase();
 localStorage.setItem('search', searchVal);
 
