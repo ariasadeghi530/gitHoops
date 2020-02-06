152e66a020a14615802836a6d4585df12484b94c
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-06 14:44:28 -0800

    added google maps functionality

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,7 +1,9 @@
-
+const apiKey = 'AIzaSyCbOrVjet_s1nbRMEgLVNsx0reP9G6Ju6g';
 
 let searchVal = localStorage.getItem('search') || '';
 
+let latit = JSON.parse(localStorage.getItem('lat')) || 33.6846;
+let long = JSON.parse(localStorage.getItem('lon')) || 117.8265;
 
 
 document.getElementById('searchBtn').addEventListener("click", event => {
@@ -12,11 +14,11 @@ document.getElementById('searchBtn').addEventListener("click", event => {
     let splitName = searchVal.split(" ");
 
     localStorage.setItem('player', splitName);
-   
+
     window.location.replace('./player.html');
   } else {
     // if team name doesn't include city, go back to previous index with city
-    if(!((nbaTeamNames.indexOf(searchVal)) % 2 === 0)){
+    if (!((nbaTeamNames.indexOf(searchVal)) % 2 === 0)) {
       searchVal = nbaTeamNames[(nbaTeamNames.indexOf(searchVal) - 1)]
       localStorage.setItem('search', searchVal);
     }
@@ -27,12 +29,11 @@ document.getElementById('searchBtn').addEventListener("click", event => {
 
 
 
-
 function searchTeam(str) {
   fetch('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + str)
     .then(r => r.json())
     .then(({ teams }) => {
-      
+      console.log(teams[0].strStadium)
       document.getElementById('teamPhoto').innerHTML = `
             <img src="${teams[0].strTeamBadge}">
         `
@@ -54,10 +55,38 @@ function searchTeam(str) {
       document.getElementById('bio').innerHTML = `
         ${teams[0].strDescriptionEN}
         `
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${teams[0].strStadium}&key=${apiKey}`)
+        .then(r => r.json())
+        .then(({ results }) => {
+          console.log(results[0].geometry.location);
+
+          latit = results[0].geometry.location.lat;
+          console.log(latit);
+          localStorage.setItem('lat', latit);
+          long = results[0].geometry.location.lng
+          console.log(long)
+          localStorage.setItem('lon', long)
+
+        })
+        .catch(e => console.error(e))
     })
     .catch(e => console.error(e));
 }
 
+function initMap() {
+
+  // The location of stadium
+
+  let stadium = { lat: latit, lng: long };
+
+  // The map, centered at locat
+
+  let map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 16, center: stadium });
+
+  // The marker, positioned at stadium
+  let marker = new google.maps.Marker({ position: stadium, map: map });
+}
 
 
 let search = localStorage.getItem('search')
