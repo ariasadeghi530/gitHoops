68ca0edd62065cb74e4c96a9c3382493fb037420
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-06 19:46:44 -0800

    populate teams iteratively instead of hard coding

--- assets/js/teams.js
+++ assets/js/teams.js
@@ -33,12 +33,23 @@ fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=nba')
     // .then(res => console.log(res))
     .then(({ teams }) => {
         for (let i = 0; i < teams.length; i++) {
-            document.getElementById(`teamPhoto${i + 1}`).innerHTML = `
+            let teamDiv = document.createElement('div');
+            teamDiv.classList = 'col s2';
+            teamDiv.innerHTML = `
+            <div id="teamPhoto${i + 1}">
             <a href="./team.html"><img src="${teams[i].strTeamBadge}" id="${teams[i].strTeam}"></a>
-        `
-            document.getElementById(`teamName${i + 1}`).innerHTML = `
-            ${teams[i].strTeam}
-        `
+            </div>
+            <h5 class="teamTitle" id="teamName${i + 1}">${teams[i].strTeam}</h5>
+            `;
+
+            if (i % 6 === 0) {
+                let divRow = document.createElement('div');
+                divRow.className = 'row';
+                document.getElementById('teamList').append(divRow);
+            }
+
+            document.getElementById('teamList').append(teamDiv);
+
         }
     })
 
