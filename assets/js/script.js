4fa8bcf6f806170c8f849d5b7f5bf060a43d1acd
EvanErickson <evanserickson@gmail.com>
2020-02-07 13:24:33 -0800

    almost finished news api work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-
-
-
 let searchVal = localStorage.getItem('search') || '';
 
 
@@ -28,4 +25,48 @@ localStorage.setItem('search', searchVal);
 document.getElementById('searchBar').value = '';
 })
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {});
+});
+
+
+
+
+
+//News api articles
+fetch(`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=f7435c3676b34c89bdb1562b6cac0849`)
+.then(r => r.json())
+.then(({articles}) => {
+  console.log(articles)
+  document.getElementById('carouselImg0').innerHTML = `
+  <img style="width: 100%;" src="${articles[0].urlToImage}">
+  `
+  document.getElementById('carouselContent0').innerHTML = `
+  <h4>${articles[0].title}</h4>
+  <p>${articles[0].description}</p>
+  `
+  document.getElementById('carouselImg1').innerHTML = `
+  <img style="width: 100%;" src="${articles[1].urlToImage}">
+  `
+  document.getElementById('carouselContent1').innerHTML = `
+  <h4>${articles[1].title}</h4>
+  <p>${articles[1].description}</p>
+  `
+  document.getElementById('carouselImg2').innerHTML = `
+  <img style="width: 100%;" src="${articles[2].urlToImage}">
+  `
+  document.getElementById('carouselContent2').innerHTML = `
+  <h4>${articles[2].title}</h4>
+  <p>${articles[2].description}</p>
+  `
+  document.getElementById('carouselImg3').innerHTML = `
+  <img style="width: 100%;" src="${articles[3].urlToImage}">
+  `
+  document.getElementById('carouselContent3').innerHTML = `
+  <h4>${articles[3].title}</h4>
+  <p>${articles[3].description}</p>
+  `
+})
 
