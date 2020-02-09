6f06da4dc953b85715e98b3a7c694d7ee164ca31
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-08 20:55:52 -0800

    formatting

--- assets/js/teams.js
+++ assets/js/teams.js
@@ -1,31 +1,5 @@
-
-
+// get search value from local storage
 let searchVal = localStorage.getItem('search') || '';
-
-
-//search bar redirect
-document.getElementById('searchBtn').addEventListener("click", event => {
-
-    searchVal = document.getElementById("searchBar").value.toLowerCase();
-    localStorage.setItem('search', searchVal);
-
-    if (!(nbaTeamNames.includes(searchVal)) && (currentPlayers.includes(searchVal))) {
-        //split name for ajax request
-        let splitName = searchVal.split(" ");
-
-        localStorage.setItem('player', splitName);
-
-        window.location.replace('./player.html')
-    } else {
-        // if team name doesn't include city, go back to previous index with city
-        if (!((nbaTeamNames.indexOf(searchVal)) % 2 === 0)) {
-            searchVal = nbaTeamNames[(nbaTeamNames.indexOf(searchVal) - 1)];
-            localStorage.setItem('search', searchVal);
-        }
-        window.location.replace('./team.html');
-    }
-    document.getElementById('searchBar').value = '';
-})
 
 //list all the teams badges on page
 fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=nba')
@@ -51,7 +25,32 @@ fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=nba')
             document.getElementById('teamList').append(teamDiv);
 
         }
-    })
+    });
+
+
+//search bar redirect
+document.getElementById('searchBtn').addEventListener("click", event => {
+
+    searchVal = document.getElementById("searchBar").value.toLowerCase();
+    localStorage.setItem('search', searchVal);
+
+    if (!(nbaTeamNames.includes(searchVal)) && (currentPlayers.includes(searchVal))) {
+        //split name for ajax request
+        let splitName = searchVal.split(" ");
+
+        localStorage.setItem('player', splitName);
+
+        window.location.replace('./player.html')
+    } else {
+        // if team name doesn't include city, go back to previous index with city
+        if (!((nbaTeamNames.indexOf(searchVal)) % 2 === 0)) {
+            searchVal = nbaTeamNames[(nbaTeamNames.indexOf(searchVal) - 1)];
+            localStorage.setItem('search', searchVal);
+        }
+        window.location.replace('./team.html');
+    }
+    document.getElementById('searchBar').value = '';
+});
 
 //clicking on team image redirects to team page
 document.getElementById('teamList').addEventListener('click', event => {
