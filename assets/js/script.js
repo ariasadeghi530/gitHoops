6c3724cbe148d7b155b10ceb8c9c13e6e908f939
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-08 17:50:33 -0800

    switched from news api to espn api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// get search value from local storage
 let searchVal = localStorage.getItem('search') || '';
 
 
@@ -7,6 +8,7 @@ document.getElementById('searchBtn').addEventListener("click", event => {
   searchVal = document.getElementById("searchBar").value.toLowerCase();
   localStorage.setItem('search', searchVal);
 
+  //change page to player page if name is in array of players
   if (!(nbaTeamNames.includes(searchVal)) && (currentPlayers.includes(searchVal))) {
     //split name for ajax request
     let splitName = searchVal.split(" ");
@@ -15,6 +17,7 @@ document.getElementById('searchBtn').addEventListener("click", event => {
 
     window.location.replace('./player.html')
   } else {
+
     // if team name doesn't include city, go back to previous index with city
     if (!((nbaTeamNames.indexOf(searchVal)) % 2 === 0)) {
       searchVal = nbaTeamNames[(nbaTeamNames.indexOf(searchVal) - 1)];
@@ -31,8 +34,8 @@ document.addEventListener('DOMContentLoaded', function () {
   let instances = M.Sidenav.init(elems, 'left');
 });
 
-
-document.addEventListener('DOMContentLoaded', function() {
+// js for carousel animation
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, {});
 });
@@ -43,45 +46,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //News api articles
 fetch(`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=f7435c3676b34c89bdb1562b6cac0849`)
-.then(r => r.json())
-.then(({articles}) => {
-  
-  document.getElementById('carouselImg0').innerHTML = `
-  <img style="width: 100%;" src="${articles[0].urlToImage}">
-  `
-  document.getElementById('carouselContent0').innerHTML = `
-  <h4>${articles[0].title}</h4>
-  <p>${articles[0].description}</p>
-  `
-  document.getElementById('carouselImg1').innerHTML = `
-  <img style="width: 100%;" src="${articles[1].urlToImage}">
-  `
-  document.getElementById('carouselContent1').innerHTML = `
-  <h4>${articles[1].title}</h4>
-  <p>${articles[1].description}</p>
-  `
-  document.getElementById('carouselImg2').innerHTML = `
-  <img style="width: 100%;" src="${articles[2].urlToImage}">
-  `
-  document.getElementById('carouselContent2').innerHTML = `
-  <h4>${articles[2].title}</h4>
-  <p>${articles[2].description}</p>
-  `
-  document.getElementById('carouselImg3').innerHTML = `
-  <img style="width: 100%;" src="${articles[3].urlToImage}">
-  `
-  document.getElementById('carouselContent3').innerHTML = `
-  <h4>${articles[3].title}</h4>
-  <p>${articles[3].description}</p>
-  `
-})
+  .then(r => r.json())
+  .then(({ articles }) => {
+  })
 
-
+//espn api for nba related articles
 fetch(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news`)
   .then(r => r.json())
-  .then( (data) => {
-    console.log(data);
+  .then(({ articles }) => {
+    console.log(articles);
+    for (let i = 0; i < 4; i++) {
+      document.getElementById(`carouselImg${i}`).innerHTML = `
+  <img style="width: 100%; height: 275px;" src="${articles[i].images[0].url}" alt="${articles[i].images[0].caption}">
+  `
+      document.getElementById(`carouselContent${i}`).innerHTML = `
+  <h4>${articles[i].headline}</h4>
+  <p>${articles[i].description}</p>
+  `
+    }
   })
-  .catch(e => console.error(e))
+  .catch(e => console.error(e));
 
 
